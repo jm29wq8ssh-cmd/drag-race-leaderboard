c34859df5e68b7a67a0a4de855976860d3afad81
jm29wq8ssh-cmd <jm29wq8ssh@privaterelay.appleid.com>
2026-03-13 17:29:11 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,45 @@
+const queens = [
+
+{
+name: "Brigiding",
+country: "Philippines",
+ppe: 4.70
+},
+
+{
+name: "Viñas Deluxe",
+country: "Philippines",
+ppe: 4.40
+},
+
+{
+name: "Tia Kofi",
+country: "UK",
+ppe: 4.25
+},
+
+{
+name: "Alexis Mateo",
+country: "USA",
+ppe: 4.00
+},
+
+{
+name: "Kween Kong",
+country: "Down Under",
+ppe: 3.92
+},
+
+{
+name: "Alyssa Edwards",
+country: "USA",
+ppe: 3.67
+},
+
+{
+name: "Ra'Jah O'Hara",
+country: "USA",
+ppe: 3.83
+}
+
+];
